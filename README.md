ad37fc7f1727ed9d16caf89c68edf1d37de5c406
kaitozu <43519768+kaitoz11@users.noreply.github.com>
2023-11-19 15:59:51 +0700

    clean up and add some description to readme

--- README.md
+++ README.md
@@ -1,22 +1,48 @@
-
 # Minaris
+My wife can do any fukin' thing.
 
+## Shooter mode
 
-## Shooting
+shoot:
+```
+minaris shooter -f [workflow file]
+```
 
+Workflow file example:
+```yaml
+name: example workflow
+workflows:
+  - name: Load facts
+    # run the "fact" module 
+    fact:
+      raw:
+        - KEY=VALUE
 
-file
-{
-    blocks[
-        request{
-            name
-            input[
-                key:val(regex) <- replace in raw
-            ]
-            output[
-                key:val(regex)
-            ]
-            raw
-        }
-    ]    
-}
+  - name: Login
+    # run the "shoot" module
+    shoot:
+      file: ./vanieva/login.k4it0z11
+```
+
+Raw flow file:
+```
+label_1
+====minaris====
+[[string selected]]--->"user":"0938194095"
+"captchaValue":"zvdg8"--->"captchaValue":"3gke3"
+"captchaToken":"f86b9833-26fa-1c26-6c57-a78486c47d24"--->"captchaToken":"ad7cc79d-18d1-7aa8-1135-49e248d01751"
+====minaris====
+LOGIN_TOKEN="token":"([a-z0-9\-]{36})"
+USER_PHONE="user":"(\d+)"
+====minaris====
+[[raw request]]
+====k4it0z11====
+label_2
+====minaris====
+[[string_befor]]--->"user":"{{USER_PHONE}}"
+"token":"2dcbc544-652d-4c41-8ca6-c3b25341f21c"--->"token":"{{LOGIN_TOKEN}}"
+====minaris====
+VARNAME=[[regex]]
+====minaris====
+[[raw request]]
+```
--- cmd/root.go
+++ cmd/root.go
@@ -10,8 +10,8 @@ import (
 
 var (
 	// Used for flags.
-	cfgFile     string
-	userLicense string
+	cfgFile string
+	// userLicense string
 
 	rootCmd = &cobra.Command{
 		Use:   "minaris",
@@ -29,13 +29,13 @@ func init() {
 	cobra.OnInitialize(initConfig)
 
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.cobra.yaml)")
-	rootCmd.PersistentFlags().StringP("author", "a", "k4it0z11", "author name for copyright attribution")
-	rootCmd.PersistentFlags().StringVarP(&userLicense, "license", "l", "", "name of license for the project")
-	rootCmd.PersistentFlags().Bool("viper", true, "use Viper for configuration")
-	viper.BindPFlag("author", rootCmd.PersistentFlags().Lookup("author"))
-	viper.BindPFlag("useViper", rootCmd.PersistentFlags().Lookup("viper"))
-	viper.SetDefault("author", "NAME HERE <EMAIL ADDRESS>")
-	viper.SetDefault("license", "apache")
+	// rootCmd.PersistentFlags().StringP("author", "a", "k4it0z11", "author name for copyright attribution")
+	// rootCmd.PersistentFlags().StringVarP(&userLicense, "license", "l", "", "name of license for the project")
+	// rootCmd.PersistentFlags().Bool("viper", true, "use Viper for configuration")
+	// viper.BindPFlag("author", rootCmd.PersistentFlags().Lookup("author"))
+	// viper.BindPFlag("useViper", rootCmd.PersistentFlags().Lookup("viper"))
+	// viper.SetDefault("author", "NAME HERE <EMAIL ADDRESS>")
+	// viper.SetDefault("license", "apache")
 
 	// rootCmd.AddCommand(addCmd)
 	// rootCmd.AddCommand(initCmd)
